35b1287bdc6bce7c107fc6f865179308fb990a4c
Kris Sistrunk <thesistrunks@googlemail.com>
2016-12-01 06:07:58 +0000

    Changes from Views service

---
+++ miet4/components/reklamationView/index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+app.reklamationView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
+
+// START_CUSTOM_CODE_reklamationView
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+
+// END_CUSTOM_CODE_reklamationView
+(function(parent) {
+    var
+    /// start global model properties
+    /// end global model properties
+        reklamationViewModel = kendo.observable({
+        submit: function() {},
+        /// start add model functions
+        /// end add model functions
+
+        cancel: function() {}
+    });
+
+    /// start form functions
+    /// end form functions
+
+    parent.set('onShow', function _onShow() {
+        var that = parent;
+        that.set('addFormData', {
+            /// start add form data init
+            /// end add form data init
+        });
+        /// start add form show
+        /// end add form show
+    });
+    parent.set('reklamationViewModel', reklamationViewModel);
+})(app.reklamationView);
+
+// START_CUSTOM_CODE_reklamationViewModel
+// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
+var pictureSource;   // picture source
+    var destinationType; // sets the format of returned value 
+     document.addEventListener("deviceready",onDeviceReady,false);
+
+     function onDeviceReady() {
+        pictureSource=navigator.camera.PictureSourceType;
+        destinationType=navigator.camera.DestinationType;
+    }
+ function onPhotoDataSuccess(imageData) {
+      // Uncomment to view the base64 encoded image data
+      // console.log(imageData);
+
+      // Get image handle
+      //
+      var smallImage = document.getElementById('smallImage');
+
+      // Unhide image elements
+      //
+      smallImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      smallImage.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    // Called when a photo is successfully retrieved
+    //
+    function onPhotoURISuccess(imageURI) {
+      // Uncomment to view the image file URI 
+      // console.log(imageURI);
+
+      // Get image handle
+      //
+      var largeImage = document.getElementById('largeImage');
+
+      // Unhide image elements
+      //
+      largeImage.style.display = 'block';
+
+      // Show the captured photo
+      // The inline CSS rules are used to resize the image
+      //
+      largeImage.src = imageURI;
+    }
+
+    // A button will call this function
+    //
+    function capturePhoto() {
+      // Take picture using device camera and retrieve image as base64-encoded string
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function capturePhotoEdit() {
+      // Take picture using device camera, allow edit, and retrieve image as base64-encoded string  
+      navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
+        destinationType: destinationType.DATA_URL });
+    }
+
+    // A button will call this function
+    //
+    function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    // Called if something bad happens.
+    // 
+    function onFail(message) {
+      alert('Failed because: ' + message);
+    }
+// END_CUSTOM_CODE_reklamationViewModel
